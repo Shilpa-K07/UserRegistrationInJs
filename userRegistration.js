f86add0f62f0c9f5e96def45e0e6c558e84e8199
shilpa <shilpa07udupi@gmail.com>
2020-11-16 11:53:53 +0530

    feat: validate first name

--- userRegistration.js
+++ userRegistration.js
@@ -1,1 +1,18 @@
 console.log("Welcome to User Registration !");
+
+const NAME_PATTERN = /^[A-Z][A-Za-z]{2,}$/;
+
+const input = require('readline-sync');
+const validate = (pattern, input) => pattern.test(input);
+
+/**
+ *  @description First name validation
+ */
+var validateFirstName = function(){
+    firstName = input.question("Enter your first name: ");
+    if(validate(NAME_PATTERN, firstName))
+        console.log("First name is valid !");
+    else
+        console.log("First name is invalid");
+}
+validateFirstName();
